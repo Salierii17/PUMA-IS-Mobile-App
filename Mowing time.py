8887eab6005b36c917dcf85c981d39f2c458a01c
EMIYAID <sulthanza06@gmail.com>
2022-09-26 23:49:59 +0700

    Hello Friend

---
+++ Mowing time.py
@@ -0,0 +1,13 @@
+A = 60  
+B = 20
+C = 30
+D = 10
+E = 2
+print(" l_block : ", A, "m" )
+print(" w_block : ", B, "m" )
+print(" l_home  : ", C, "m" )
+print(" w_home  : ", D, "m" )
+Area = (A*B) - (C*D)
+print(" Area : ", Area, "m^2")
+print(" mow_spd : ", E, "m^2/s" )
+print ("Mowing time : ", Area/2, "s")
---
+++ Progamming Concept.py
@@ -0,0 +1,106 @@
+
+
+x = 1
+y = 2
+z = 3
+print (x+y+z)
+6
+result = x + y + z
+Traceback (most recent call last)
+Traceback (most recent call last):
+    Python Shell, prompt 8, line 1
+Syntax Error: invalid syntax. Perhaps you forgot a comma?: <string>, line 1, pos 12
+result = x + y + z
+print (result)
+6
+
+x = 100
+x
+100
+x = "Hello!"
+x
+'Hello!'
+y = 10
+print (x * y )
+Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!
+print (x/y)
+Traceback (most recent call last):
+    Python Shell, prompt 17, line 1
+builtins.TypeError: unsupported operand type(s) for /: 'str' and 'int'
+x = 67 
+print(x/y)
+6.7
+x1234=100
+1x = 3
+Traceback (most recent call last):
+    Python Shell, prompt 21, line 1
+Syntax Error: invalid decimal literal: <string>, line 1, pos 1
+_test = 9
+_12letter = 6
+d351 = 55
+x = 3
+prit (x)
+Traceback (most recent call last):
+    Python Shell, prompt 26, line 1
+builtins.NameError: name 'prit' is not defined
+print(x)
+3
+oneprogram = "Hello"
+print (oneprogram)
+Hello
+
+x = 9
+y = 10
+z = x + y 
+z = x**3
+z
+729
+x = 4 
+y = 6
+print(x**3+y**2)
+100
+x = 2
+y = 3
+z = 4
+n = x+y * z
+n
+14
+print (x,y,z,n)
+2 3 4 14
+print()
+
+counter = 0
+counter = counter + 1
+counter
+1
+counter = counter + 1
+counter
+2
+counter = counter + 11
+counter
+13
+counter = counter - 5
+counter
+8
+
+myvar = eval(input("Enter One Number : "))
+Enter One Number : 76
+myvar
+76
+ myvar=input("Enter your name : "))
+Traceback (most recent call last):
+  Python Shell, prompt 56, line 1
+Syntax Error: unmatched ')': <string>, line 1, pos 34
+myvar=input("Enter your name : ")
+Enter your name : Jhony
+myvar
+'Jhony'
+x, y, z = 1,2,3
+print (x,y,z)
+1 2 3
+x
+1
+y
+2
+z
+3 
